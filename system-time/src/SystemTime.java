5e29a8ea4c3867c0ac59123cfa952c6054a85734
Lý Thanh Tùng <lythanhtung.work@gmail.com>
2022-08-09 11:19:02 +0700

    system time

---
+++ system-time/src/SystemTime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main (String [] args)
+    {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+        System.out.println("Now is: " + now);
+    }
+}
